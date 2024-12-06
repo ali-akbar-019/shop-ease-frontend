db566f5069031190cec2fdbca818f341c6cc2179
ali akbar <gamesforever018@gmail.com>
2024-12-05 16:47:02 -0800

    progress folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import HomePage from "./page/HomePage"
-import AboutPage from "./page/AboutPage"
-import ContactPage from "./page/ContactPage"
-import ProfilePage from "./page/ProfilePage"
-import ProductsPage from "./page/ProductsPage"
-import SingleProductPage from "./page/SingleProductPage"
-import BillingPage from "./page/BillingPage"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./page/HomePage";
+import AboutPage from "./page/AboutPage";
+import ContactPage from "./page/ContactPage";
+import ProfilePage from "./page/ProfilePage";
+import ProductsPage from "./page/ProductsPage";
+import SingleProductPage from "./page/SingleProductPage";
+import BillingPage from "./page/BillingPage";
+import AdminHomePage from "./page/Admin/HomePage";
 
 const App = () => {
   return (
-   <>
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<HomePage/>}/>
-    <Route path="/about" element={<AboutPage />}/>
-    <Route path="/contact" element={<ContactPage />}/>
-    <Route path="/products" element={<ProductsPage />}/>
-    <Route path="/products/:id" element={<SingleProductPage />}/>
-    <Route path="/billing" element={<BillingPage />}/>
-    <Route path="/profile" element={<ProfilePage />}/>
-   </Routes>
-   
-   </BrowserRouter>
-   
-   </>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<SingleProductPage />} />
+          <Route path="/billing" element={<BillingPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/admin" element={<AdminHomePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<p>404 Not Found!</p>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
